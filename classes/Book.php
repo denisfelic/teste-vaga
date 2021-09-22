7e243c64e531491c314d304206a52ea3c817515e
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-09-21 21:21:22 -0300

    add book class

--- classes/Book.php
+++ classes/Book.php
@@ -1,7 +1,28 @@
 <?php
 
-class Book
+
+require_once __DIR__ . './Readable.php';
+require_once __DIR__ . './iItem.php';
+
+class Book extends Readable implements iItem
 {
-    public $title;
-    public $author;
+    private string $title;
+    private string $author;
+
+    public function __construct(string $title, string $author, array $pages)
+    {
+        parent::__construct($pages);
+        $this->title = $title;
+        $this->author = $author;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
 }
